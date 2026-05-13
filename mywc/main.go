596bdca70f46a09d35cb92148a2daed883c6fcf1
maxdiplogit <maxdiplogit@gmail.com>
2026-05-14 01:30:42 +0530

    feature: basic mywc, read from file and print lines and words

---
+++ mywc/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	// "flag"
+)
+
+// This tool reimplements UNIX's wc binary in Go
+// It reads either from the stdin/command-line-args or from a file
+// Currently it has only two flags -w (count only words) and -l (count only lines)
+
+func main() {
+	// cli_args := os.Args
+
+	// fmt.Printf("CLI Args: %#v\n", cli_args)
+
+	// for i := 0; i < len(cli_args); i++ {
+	// 	fmt.Printf("Arg %d: %s\n", i, cli_args[i])
+	// 	fmt.Printf("Arg length %d: %d\n", i, len(cli_args[i]))
+	// 	for j := 0; j < len(cli_args[i]); j++ {
+	// 		fmt.Printf("Byte: %#v\n", cli_args[i][j])
+	// 	}
+	// }
+
+	home, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		// os.Exit does not run deferred functions.
+		os.Exit(1)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: ./mywc <filename>")
+		// os.Exit does not run deferred functions.
+		os.Exit(1)
+	}
+
+	filePath := filepath.Join(home, os.Args[1])
+
+	fmt.Printf("FilePath: %s\n", filePath)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines, words int = 0, 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		// fmt.Println("Line ", lines, ": ", line)
+
+		wordsFromString := strings.Split(line, " ")
+		// fmt.Printf("wordsFromString: %#v\n", wordsFromString)
+
+		words += len(wordsFromString)
+
+		lines++
+	}
+
+	fmt.Println("Words: ", words)
+	fmt.Println("Lines: ", lines)
+}
